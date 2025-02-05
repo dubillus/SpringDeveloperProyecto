0e589ea8c0108c6364986746327aae06ec44997a
Darwin <darwin.ubillus@hotmail.com>
2025-02-05 01:54:30 -0500

    first commit

---
+++ proyectofinal-client/src/test/java/com/galaxy/proyectofinal_client/ProyectofinalClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galaxy.proyectofinal_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectofinalClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
